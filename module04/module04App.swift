9e6a55e19e48b8f67d1f79f499e82c20898fc39d
Lucasnguyen <whoisgotyourback@gmail.com>
2022-02-07 20:48:25 +0700

    Initial Commit

---
+++ module04/module04App.swift
@@ -0,0 +1,17 @@
+//
+//  module04App.swift
+//  module04
+//
+//  Created by Lucas Nguyen on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct module04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
